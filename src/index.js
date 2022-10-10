3040729b7baf86f489aed55a6694e9c2fd3fbb99
sarkermajid <majidursarkar333@gmail.com>
2022-10-10 18:09:43 +0600

    JSX and JS expression

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+const heading = 'Todo App';
+const date = new Date();
+const currentDate = date.getDate();
+const currentMonth = date.getMonth();
+const currentYear = date.getFullYear();
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <h1>Welcome ReactJs</h1>
+  <div>
+      <h1>{heading}</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt optio nisi maxime rerum quibusdam, corrupti enim fuga voluptatem soluta mollitia.</p>
+      <p>{currentDate + '/' + currentMonth + '/' + currentYear}</p>
+      <button>Click Here</button>
+  </div>
+
 );
