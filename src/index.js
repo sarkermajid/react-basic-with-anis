936b9f45a08951eb4dc780e16861debc4cce2497
sarkermajid <majidursarkar333@gmail.com>
2022-10-10 18:40:36 +0600

    more css in JSX

--- src/index.js
+++ src/index.js
@@ -7,34 +7,17 @@ const currentDate = date.getDate();
 const currentMonth = date.getMonth();
 const currentYear = date.getFullYear();
 
-const cardStyle = {
-  backgroundColor: 'black',
-  width:'300px',
-  height:'300px',
-  padding: '10px',
-  borderRadius:'10px',
-  border:'2px solid red',
-  color:'yellow',
-  textAlign:'center'
-}
-
-const buttonStyle = {
-  backgroundColor:'yellow',
-  color:'red',
-  fontSize:'18px',
-  padding:'5px',
-  borderRadius:'5px',
-  border:'2px solid red'
-}
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div style={cardStyle}>
-      <h1>{heading}</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt optio nisi maxime rerum quibusdam, corrupti enim fuga voluptatem soluta mollitia.</p>
-      <p>{currentDate + '/' + currentMonth + '/' + currentYear}</p>
-      <button style={buttonStyle}>Click Here</button>
+  <div>
+      <h1 className='headingStyle'>{heading}</h1>
+      <div className='card'>
+        <h4 className='cardTitle'>Call Family</h4>
+        <p className='cardDescription'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt optio nisi maxime rerum quibusdam, corrupti enim fuga voluptatem soluta mollitia.</p>
+        <p className='cardDate'>{currentDate + '/' + currentMonth + '/' + currentYear}</p>
+        <button className='buttonStyle'>Click Here</button>
+      </div>
   </div>
 
 );
