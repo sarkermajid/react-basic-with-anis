d09b9441e9aea9fc62009f9a6127e86244b47758
sarkermajid <majidursarkar333@gmail.com>
2022-10-10 18:12:47 +0600

    styling in JSX

--- src/index.js
+++ src/index.js
@@ -7,14 +7,34 @@ const currentDate = date.getDate();
 const currentMonth = date.getMonth();
 const currentYear = date.getFullYear();
 
+const cardStyle = {
+  backgroundColor: 'black',
+  width:'300px',
+  height:'300px',
+  padding: '10px',
+  borderRadius:'10px',
+  border:'2px solid red',
+  color:'yellow',
+  textAlign:'center'
+}
+
+const buttonStyle = {
+  backgroundColor:'yellow',
+  color:'red',
+  fontSize:'18px',
+  padding:'5px',
+  borderRadius:'5px',
+  border:'2px solid red'
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div>
+  <div style={cardStyle}>
       <h1>{heading}</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt optio nisi maxime rerum quibusdam, corrupti enim fuga voluptatem soluta mollitia.</p>
       <p>{currentDate + '/' + currentMonth + '/' + currentYear}</p>
-      <button>Click Here</button>
+      <button style={buttonStyle}>Click Here</button>
   </div>
 
 );
