e823ff3d6289320401b1a566cf49e1309d4a2d54
sarkermajid <majidursarkar333@gmail.com>
2022-10-15 12:25:30 +0600

    React icon

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Card from "./components/Card";
 import cardData from "./data";
 import Card2 from "./components/card2";
+import { FaReact, FaYoutube, FaFacebookMessenger } from "react-icons/fa";
 
 const heading = 'Todo App';
 function App(){
@@ -10,6 +11,9 @@ function App(){
             <h1 className='headingStyle'>{heading}</h1>
             {cardData.map(data => <Card key={data.id} title={data.title} des={data.des}></Card>)}
             <Card2 name='Sarker Majid'></Card2>
+            <FaReact className="icon"></FaReact>
+            <FaYoutube className="icon"></FaYoutube>
+            <FaFacebookMessenger className="icon"></FaFacebookMessenger>
         </div>
     );
 }
